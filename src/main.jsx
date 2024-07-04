b828779e02caa8faa9ae805fe5dd1737ec1bc5e2
rasyid <rasyiddmuhamma12@gmail.com>
2024-07-04 10:29:03 +0700

    r

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import "./dist/main.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponen from "./components/NavbarComponen";
+import FooterComponen from "./components/FooterComponen";
+import { Routes, Route } from "react-router-dom";
+import HomePages from "./pages/HomePages";
+import RoadmapsPages from "./pages/RoadmapsPages";
+import CategoryPages from "./pages/CategoryPages";
+import materi1html from "./pages/materi1html";
+import materi2html from "./pages/materi2html";
+import materi3html from "./pages/materi3html";
+import materi1css from "./pages/materi1css";
+import materi2css from "./pages/materi2css";
+import materi3css from "./pages/materi3css";
+import materi1js from "./pages/materi1js";
+import materi2js from "./pages/materi2js";
+import materi3js from "./pages/materi3js";
+import ajax from "./pages/ajax";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/Tugas_Akhir",
+        element: <HomePages />,
+      },
+      {
+        path: "/Tugas_Akhir/category",
+        element: <CategoryPages />,
+      },
+      {
+        path: "/Tugas_Akhir/Roadmaps",
+        element: <RoadmapsPages />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
